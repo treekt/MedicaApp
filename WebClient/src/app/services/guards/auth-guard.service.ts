869ed3af25996063e06c88f8a99ffa9728674ee6
Łukasz Trzciński <lukasz.trzcinski@softiq.pl>
2018-11-13 17:45:06 +0100

    Page not found and auth guard

---
+++ WebClient/src/app/services/guards/auth-guard.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthService} from '../auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.authService.isTokenExpired()) {
+      return true;
+    }
+    this.router.navigate(['/login']);
+    return false;
+  }
+}
+
