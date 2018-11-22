7492809e3b3aaddd0fa8df8be20b15655048d010
Łukasz Trzciński <lukasz.trzcinski@softiq.pl>
2018-11-23 00:52:25 +0100

    Created visit-management components

---
+++ WebClient/src/app/core/visit/visit-management.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visit-management',
+  templateUrl: './visit-management.component.html'
+})
+export class VisitManagementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
